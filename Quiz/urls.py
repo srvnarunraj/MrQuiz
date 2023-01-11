1eace9eedb7dfff50b1964c5c624c2bc03c88ebb
Arun Raj <arunraj14092002@gmail.com>
2023-01-11 17:22:19 +0530

    Updated UI Actions

--- Quiz/urls.py
+++ Quiz/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('phy',views.phy),
     path('chem',views.chem),
     path('math',views.math),
-    path('phy-<int:q>',views.phy),
-    path('chem-<int:q>',views.chem),
-    path('math-<int:q>',views.math),
+    path('phy/<int:q>',views.phy),
+    path('chem/<int:q>',views.chem),
+    path('math/<int:q>',views.math),
     path('submitanswer',views.submitanswer)
 ]
--- Quiz/views.py
+++ Quiz/views.py
@@ -3,32 +3,53 @@ from django.shortcuts import render,redirect
 def startup(request):
     return redirect('/math')
 def phy(request,q=1):
+    x=request.path
+    if any(chr.isdigit() for chr in x):
+        pass
+    else:
+        return redirect('/phy/1') 
+    path='phy'
     if q<25:
         nextq=q+1
     else:
-        nextq=25
+        path='chem'
+        nextq=1
     mydata={
-        'path':'phy',
+        'path':path,
         'q':nextq,
     }
     return render(request,'s_phy.html',mydata)
 def math(request,q=1):
+    x=request.path
+    if any(chr.isdigit() for chr in x):
+        pass
+    else:
+        return redirect('/math/1')
+    path ='math'
     if q<25:
         nextq=q+1
     else:
-        nextq=25
+        path='phy'
+        nextq=1
     mydata={
-        'path':'math',
+        'path':path,
         'q':nextq,
     }
     return render(request,'s_math.html',mydata)
 def chem(request,q=1):
+    x=request.path
+    if any(chr.isdigit() for chr in x):
+        pass
+    else:
+        return redirect('/chem/1')
+
+    path = 'chem'
     if q<25:
         nextq=q+1
     else:
-        nextq=25
+        nextq=1
     mydata={
-        'path':'chem',
+        'path':path,
         'q':nextq,
     }
     return render(request,'s_chem.html',mydata)
